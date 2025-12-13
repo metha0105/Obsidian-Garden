61be45c9e44adb143829e398ad6e97be392453f5
Metha T <92750427+metha0105@users.noreply.github.com>
2025-12-12 23:01:56 -0500

    Additional changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = 
-    import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+const API_BASE_URL = import.meta.env.PROD
+    ? "https://obsidian-garden-y1mf.onrender.com"
+    : "http://localhost:8080";
 
-export { API_BASE_URL };
+export { API_BASE_URL }
